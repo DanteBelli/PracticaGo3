21a61518b74973f8d71bc61f2ec86a0951067ff6
DanteBell95 <dante.belli18@gmail.com>
2025-02-27 12:14:24 -0300

    Modif+commentarios

--- main.go
+++ main.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+/*Contraint utilizado para decirle a la funcion q tipo de datos puede recibir*/
+type Number interface {
+	int64 | float64
+}
+
 func main() {
 	ints := map[string]int64{
 		"primero": 11,
@@ -17,6 +22,7 @@ func main() {
 	fmt.Printf("Suma sabiendo tipo de dato %v y %v\n",
 		sumTodo(ints),
 		sumTodo(floats))
+	fmt.Printf("La suma Generica de los Constraint es de : %v y %v\n", sumTodoGen(ints), sumTodoGen(floats))
 }
 func sumInt(m map[string]int64) int64 {
 	var s int64
@@ -33,6 +39,13 @@ func sumFloat(m map[string]float64) float64 {
 	return s
 }
 func sumTodo[K comparable, V int64 | float64](m map[K]V) V {
+	var s V
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+func sumTodoGen[K comparable, V Number](m map[K]V) V {
 	var s V
 	for _, v := range m {
 		s += v
